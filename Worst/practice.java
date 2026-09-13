32df29a28d994030c27238180eed2b867383beca
Md-meraj21 <mdmerajansari975@gmail.com>
2026-09-13 23:25:50 +0530

    revise Star pattern problems

--- Worst/practice.java
+++ Worst/practice.java
@@ -1,7 +1,17 @@
 // package Worst;
 
 public class practice {
-    
+    public static void main(String[] args) {
+        int n = 4;
+        for (int row = 1; row <= n; row++) { 
+            // print for each row
+            for (int column = 1; column <= n; column++) {
+                //print for each column 
+                System.out.print("+");
+                //print + while check for loop
+            }
+            System.out.println();
+        }
+    }
 
-    
 }
